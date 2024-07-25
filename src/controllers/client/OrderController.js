eef6a126187464ef8252318120407532c6c6aec4
tmi.tutaa <tmi.tutaa@gmail.com>
2024-07-25 22:17:03 +0700

    commit:update discount warehouse

--- src/controllers/client/OrderController.js
+++ src/controllers/client/OrderController.js
@@ -1,10 +1,11 @@
 const Type = require('../../models/Type');
 const Order = require("../../models/Order");
+const Warehouse = require("../../models/Warehouse");
 
 const index = async(req, res) => {     
     const types = await Type.find();  
     const userId = req.user.id;
-    const orders = await Order.find({ user : userId }).exec();
+    const orders = await Order.find({ user : userId }).sort({ date: -1 });
     res.render('client/order/index', {
         title: 'Đơn hàng', 
         types: types,
@@ -16,7 +17,17 @@ const updateStatus = async(req, res) => {
     try {
         const { status, payment_status } = req.body;
         const id = req.params.id;
-        const statusOrder = await Order.findByIdAndUpdate(id, { payment_status: payment_status, status: status }, { new: true });
+        const order = await Order.findByIdAndUpdate(id, { payment_status: payment_status, status: status }, { new: true });
+        if (status == 4) {
+            const updatePromises = Object.entries(order.cart.items).map(([key, item]) => {
+                return Warehouse.findOneAndUpdate(
+                    { product_id: item.item._id },
+                    { $inc: { quantity: -item.qty } },
+                    { new: true }
+                );
+            });
+            await Promise.all(updatePromises);
+        }
         res.redirect('back');
     } catch (e) {
         console.log(e);
--- src/controllers/client/ProductController.js
+++ src/controllers/client/ProductController.js
@@ -3,6 +3,7 @@ const Type = require('../../models/Type');
 const ImageProduct = require('../../models/ImageProduct');
 const ProductAttribute = require('../../models/ProductAttribute');
 const ProductColor = require('../../models/ProductColor');
+const Review = require('../../models/Review');
 
 const show = async(req,res) => {
     id = req.params.id;
@@ -10,12 +11,14 @@ const show = async(req,res) => {
     const images = await ImageProduct.find({ product_id : id }).populate([{path: 'product_id'}]).exec();
     const attributes = await ProductAttribute.find({ product_id : id }).populate([{path: 'attribute_id'}]).exec();
     const colors = await ProductColor.find({ product_id : id }).populate([{path: 'color_id'}]).exec();
+    const reviews = await Review.find({ product_id : id }).populate([{path: 'product_id'}]).exec();
     const types = await Type.find(); 
     res.render('client/product/show', {
         product: product, 
         images: images, 
         attributes: attributes, 
         colors: colors, 
+        reviews: reviews,
         types: types, 
         title: 'Product detail',
         layout: 'client/layout/main',
